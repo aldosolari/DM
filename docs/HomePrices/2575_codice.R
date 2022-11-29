900af4c07dee7925bc23ce5d0f68f35bd04ea85b
Aldo Solari <solari.aldo@gmail.com>
2022-11-29 23:58:35 +0100

    update

--- docs/HomePrices/2575_codice.R
+++
@@ -1,8 +0,0 @@
-library(readr)
-library(tidymodels)
-PATH <- "https://raw.githubusercontent.com/aldosolari/DM/master/docs/HomePrices/"
-train = read_csv2(paste0(PATH,"home_prices_train.csv"))
-test = read_csv2(paste0(PATH,"home_prices_test.csv"))
-fit <- linear_reg() %>% set_engine("lm") %>% fit(price ~ ., data = train)
-yhat <- predict(fit, new_data = test)
-write.table(file="2575_previsione.txt", yhat$.pred, row.names = FALSE, col.names = FALSE)
