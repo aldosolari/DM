01259aa611be41d7be4890e92a6f75be65d7f03f
Aldo Solari <solari.aldo@gmail.com>
2023-01-12 11:04:45 +0100

    update

--- docs/RCODE/Splines.R
+++ docs/RCODE/Splines.R
@@ -13,17 +13,18 @@ y = fx(x) + rnorm(n,0,0.1)
 
 #--- REGRESSIONE POLINOMIALE ----------------------------
 
-fit = lm(y ~ poly(x,degre=17, raw=T))
-#pdf("Figure_poly17.pdf")
-plot(x,y, main="Regressione polinomiale di grado 17")
+M = 10
+fit = lm(y ~ poly(x,degree=M, raw=T))
+#pdf("Figure_polyM.pdf")
+plot(x,y, main=paste("Regressione polinomiale di grado", M))
 lines(x,fitted(fit))
 #dev.off()
 
 B = model.matrix(fit)
 S = B %*% solve(crossprod(B)) %*% t(B)
 
-#pdf("Figure_hatmatrix17.pdf")
-filled.contour(main="Regressione polinomiale di grado 17",apply(S, 2, rev), asp=1, xaxt="n", yaxt="n", xlab="columns", ylab="rows", levels=c(min(S),-0.005,0.005,max(S)), col=c("red", "white", "blue"))
+#pdf("Figure_hatmatrixM.pdf")
+filled.contour(main=paste("Regressione polinomiale di grado", M), apply(S, 2, rev), asp=1, xaxt="n", yaxt="n", xlab="columns", ylab="rows", levels=c(min(S),-0.005,0.005,max(S)), col=c("red", "white", "blue"))
 #dev.off()
 
 kappa(crossprod(B), exact=T)
@@ -31,7 +32,8 @@ kappa(crossprod(B), exact=T)
 #--- POLINOMI0 A TRATTI ----------------------------
 
 M = 2
-knots = seq(-1,1,length=14)[-c(1,14)]
+K = 12 # numero di intervalli = K+1
+knots = seq(-1,1,length=K+2)[-c(1,K+2)]
 x_cut = cut(x, c(-Inf,knots,Inf) )
 #pdf("Figure_piece_poly.pdf")
 plot(x,y)
@@ -46,12 +48,13 @@ for (i in 1:length(levels(x_cut)) ){
 #dev.off()
 
 #pdf("Figure_hatmatrix_piece_poly.pdf")
-filled.contour(main="Polinomio a tratti di grado 2",apply(S, 2, rev), asp=1, xaxt="n", yaxt="n", xlab="columns", ylab="rows", levels=c(min(S),-0.005,0.005,max(S)), col=c("red", "white", "blue"))
+filled.contour(main=paste("Polinomio a tratti di grado", M),apply(S, 2, rev), asp=1, xaxt="n", yaxt="n", xlab="columns", ylab="rows", levels=c(min(S),-0.005,0.005,max(S)), col=c("red", "white", "blue"))
 #dev.off()
 
 
 #--- SPLINE DI REGRESSIONE ----------------------------
 
+# costruzione della spline con funzioni base di potenza troncata
 K = length(knots)
 n = length(x)
 B = matrix(NA,ncol=M+K+1, nrow=n)
@@ -65,19 +68,20 @@ abline(v=knots, col="gray", lty=3)
 lines(x,predict(fit), lwd=2)
 #dev.off()
 
-S = B %*% solve(crossprod(B)) %*% t(B)
-#pdf("Figure_hatmatrix_reg_spline.pdf")
-filled.contour(main="Spline di regressione di grado 2",apply(S, 2, rev), asp=1, xaxt="n", yaxt="n", xlab="columns", ylab="rows", levels=c(min(S),-0.005,0.005,max(S)), col=c("red", "white", "blue"))
-#dev.off()
-
-
 #--- RADIOCARBON DATA ----------------------------
+
+rm(list=ls())
 
 library(sm) 
 dat = radioc[(radioc$Cal.age > 2000 & radioc$Cal.age < 3000), ]
-x = dat$Cal.age
-y = dat$Rc.age
-plot(y ~ x)
+x = dat$Cal.age/1000
+y = dat$Rc.age/1000
+#pdf("Figure_radiocarbon.pdf")
+plot(y ~ x, xlab="calibrated age (1000)", ylab="radio-carbon age (1000)")
+#dev.off()
+
+
+#--- SCELTA DEL GRADO M ----------------------------
 
 K = 6
 knots = seq(min(x),max(x),length=K+2)[-c(1,K+2)]
@@ -89,12 +93,13 @@ B[,(M+2):(M+K+1)] = sapply(1:K, function(k) ifelse(x >= knots[k], (x-knots[k])^M
 fit = lm(y ~ 0 + B)
 
 #pdf("Figure_radioc_M.pdf")
-plot(x,y, ylab="Radio−carbon age", xlab="Calibrated age", main=paste("Degree", M))
+plot(x,y, ylab="Radio-carbon age", xlab="Calibrated age", main=paste("Degree", M))
 abline(v=knots, col="gray", lty=3)
 lines(x,predict(fit), lwd=2)
 #dev.off()
 
 
+#--- SCELTA DEL NUMERO K ----------------------------
 
 K = 7
 knots = seq(min(x),max(x),length=K+2)[-c(1,K+2)]
@@ -106,7 +111,7 @@ B[,(M+2):(M+K+1)] = sapply(1:K, function(k) ifelse(x >= knots[k], (x-knots[k])^M
 fit = lm(y ~ 0 + B)
 
 #pdf("Figure_radioc_K.pdf")
-plot(x,y, ylab="Radio−carbon age", xlab="Calibrated age", main=paste(K+2, "knots"))
+plot(x,y, ylab="Radio-carbon age", xlab="Calibrated age", main=paste(K, "internal knots"))
 abline(v=knots, col="gray", lty=3)
 abline(v=range(x), col="gray", lty=3)
 lines(x,predict(fit), lwd=2)
@@ -116,55 +121,46 @@ lines(x,predict(fit), lwd=2)
 
 #pdf("Figure_interpolating_ns.pdf")
 f = splinefun(x, y, method = "natural")
-plot(x, y, xlim=c(1900,3100))
-curve(f(x), add = TRUE, from = 1000, to = 4000, n = 1000, lwd=2) 
+plot(x, y, xlim=c(1.9,3.1))
+curve(f(x), add = TRUE, from = 1, to = 4, n = 1000, lwd=2) 
 #dev.off()
 
-#--- TRUNCATED POWER BASIS ----------------------------
+#--- CONSTRUCTION B-SPLINE (STANDARDIZED) -----
 
-x = (x-min(x)) / (max(x)-min(x))
-y = (y-min(y)) / (max(y)-min(y))
+xx = (x-min(x)) / (max(x)-min(x))
+yy = (y-min(y)) / (max(y)-min(y))
 
 K = 13
-knots = seq(min(x),max(x),length=K+2)[-c(1,K+2)]
+knots = seq(min(xx),max(xx),length=K+2)[-c(1,K+2)]
 M = 3
-n = length(x)
+n = length(xx)
 B = matrix(NA,ncol=M+K+1, nrow=n)
-B[,1:(M+1)] = cbind(1,poly(x,M, raw=TRUE))
-B[,(M+2):(M+K+1)] = sapply(1:K, function(k) ifelse(x >= knots[k], (x-knots[k])^M, 0))
+B[,1:(M+1)] = cbind(1,poly(xx,M, raw=TRUE))
+B[,(M+2):(M+K+1)] = sapply(1:K, function(k) ifelse(xx >= knots[k], (xx-knots[k])^M, 0))
 
 #pdf("Figure_tpowerbasis.pdf")
 op <- par(mfrow=c(3,1), mar = c(0, 4, 0, 0))
-plot(x,y, ylim=c(-0.5,1.5), col="white", ylab="Unscaled basis functions Bj(x)")
+plot(xx,yy, ylim=c(-0.5,1.5), col="white", ylab="Unscaled basis functions Bj(x)")
 abline(v=knots, lty=3, col="gray")
-for (i in 1:ncol(B)) lines(x,B[,i], col=i, lwd=2)
+for (i in 1:ncol(B)) lines(xx,B[,i], col=i, lwd=2)
 
-fit = lm(y ~ 0 + B)
+fit = lm(yy ~ 0 + B)
 beta_hat = coef(fit)
-beta_hat
 B_scaled = sapply(1:length(beta_hat),function(j) B[,j]*beta_hat[j])
 
-plot(x,y, ylim=c(-0.5,1.5), col="white", , ylab="Scaled basis functions betaj * Bj(x)")
+plot(xx,yy, ylim=c(-0.5,1.5), col="white", , ylab="Scaled basis functions betaj * Bj(x)")
 abline(v=knots, lty=3, col="gray")
-for (i in 1:ncol(B_scaled)) lines(x,B_scaled[,i], col=i, lwd=2)
+for (i in 1:ncol(B_scaled)) lines(xx,B_scaled[,i], col=i, lwd=2)
 
-plot(x,y, ylim=c(-0.5,1.5), ylab="Sum betaj * Bj(x)")
+plot(xx,yy, ylim=c(-0.5,1.5), ylab="Sum betaj * Bj(x)")
 abline(v=knots, lty=3, col="gray")
 y_hat = apply(B_scaled, 1, sum)
-lines(x,y_hat, lwd=2)
+lines(xx,y_hat, lwd=2)
 par(op)
 #dev.off()
 
 
-#--- NON-STANDARDIZED ----------------------------
-
-rm(list=ls())
-
-library(sm) 
-dat = radioc[(radioc$Cal.age > 2000 & radioc$Cal.age < 3000), ]
-x = dat$Cal.age
-y = dat$Rc.age
-
+#--- NON-STANDARDIZED DATA ----------------------------
 
 K = 13
 knots = seq(min(x),max(x),length=K+2)[-c(1,K+2)]
@@ -218,35 +214,34 @@ for (i in 1:ncol(B)) lines(x,B[,i], col=i, lwd=2)
 rowSums(B)
 
 
-x = (x-min(x)) / (max(x)-min(x))
-y = (y-min(y)) / (max(y)-min(y))
 
-xl=min(x)
-xr=max(x)
+
+#--- CONSTRUCTION B-SPLINE (STANDARDIZED) -----
+
+xl=min(xx)
+xr=max(xx)
 ndx=K+1
 bdeg=3
-
-B <- bbase(x, xl, xr, ndx, bdeg)
+BB <- bbase(xx, xl, xr, ndx, bdeg)
 
 #pdf("Figure_Bbasis.pdf")
 op <- par(mfrow=c(3,1), mar = c(0, 4, 0, 0))
-plot(x,y, ylim=c(-0.5,1.5), col="white", ylab="Unscaled basis functions Bj(x)")
+plot(xx,yy, ylim=c(-0.5,1.5), col="white", ylab="Unscaled basis functions Bj(x)")
 abline(v=knots, lty=3, col="gray")
-for (i in 1:ncol(B)) lines(x,B[,i], col=i, lwd=2)
+for (i in 1:ncol(B)) lines(xx,BB[,i], col=i, lwd=2)
 
-fit = lm(y ~ 0 + B)
+fit = lm(yy ~ 0 + BB)
 beta_hat = coef(fit)
-beta_hat
-B_scaled = sapply(1:length(beta_hat),function(j) B[,j]*beta_hat[j])
+BB_scaled = sapply(1:length(beta_hat),function(j) BB[,j]*beta_hat[j])
 
-plot(x,y, ylim=c(-0.5,1.5), col="white", , ylab="Scaled basis functions betaj * Bj(x)")
+plot(xx,yy, ylim=c(-0.5,1.5), col="white", , ylab="Scaled basis functions betaj * Bj(x)")
 abline(v=knots, lty=3, col="gray")
-for (i in 1:ncol(B_scaled)) lines(x,B_scaled[,i], col=i, lwd=2)
+for (i in 1:ncol(BB_scaled)) lines(xx,BB_scaled[,i], col=i, lwd=2)
 
-plot(x,y, ylim=c(-0.5,1.5), ylab="Sum betaj * Bj(x)")
+plot(xx,yy, ylim=c(-0.5,1.5), ylab="Sum betaj * Bj(x)")
 abline(v=knots, lty=3, col="gray")
-y_hat = apply(B_scaled, 1, sum)
-lines(x,y_hat, lwd=2)
+y_hat = apply(BB_scaled, 1, sum)
+lines(xx,y_hat, lwd=2)
 par(op)
 #dev.off()
 
@@ -256,12 +251,6 @@ par(op)
 # NATURAL CUBIC SPLINES
 #---------------------------------------
 
-rm(list=ls())
-
-library(sm) 
-dat = radioc[(radioc$Cal.age > 2000 & radioc$Cal.age < 3000), ]
-x = dat$Cal.age
-y = dat$Rc.age
 
 nat_spline_x <- function(x, knots){
   
@@ -293,14 +282,9 @@ K = 11
 knots = seq(min(x),max(x),length=K+2)[-c(1,K+2)]
 n = length(x)
   
-#pdf("Figure_nat_spline.pdf")
-plot(x,y)
-abline(v=knots, lty=3, col="gray")
+
 B <- nat_spline_x(x, knots)
 y_hat <- B %*% solve(crossprod(B)) %*% crossprod(B, y)
-lines(x,y_hat, lwd=2)
-#dev.off()
-
 
 fit_ns <- lm(y ~ 0+B)
 y_hat_ns <- predict(fit_ns, se = TRUE)
@@ -311,9 +295,17 @@ X[,(M+2):(M+K+1)] = sapply(1:K, function(k) ifelse(x >= knots[k], (x-knots[k])^M
 fit_cs <- lm(y ~ 0+X)
 y_hat_cs <- predict(fit_cs, se = TRUE)
 
+#pdf("Figure_nat_spline.pdf")
+plot(x,y)
+abline(v=knots, lty=3, col="gray")
+lines(x,y_hat, lwd=2, col=4)
+lines(x,y_hat_cs$fit, lwd=2)
+legend("top", c("natural cubic spline", "cubic spline"), lty=1, col=c(4,1), lwd=2)
+#dev.off()
+
+
 #pdf("Figure_standard_error.pdf")
 plot(x, y_hat_cs$se.fit, type="l", 
-     ylim=c(0.02,max(y_hat_cs$se.fit)),
      ylab="Standard error", lwd=2)
 lines(x, y_hat_ns$se.fit, lwd=2, col=4)
 legend("top", c("cubic spline", "natural cubic spline"), lty=1, col=c(1,4), lwd=2)
